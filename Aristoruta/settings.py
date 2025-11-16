57d8ab6a51db2c910b3dce19e659caa5a8def135
Saul333 <polloamarillo65@gmail.com>
2025-11-15 22:33:15 -0600

    Agregar archivos de configuración y plantillas para el proyecto AristoRutas

--- Aristoruta/settings.py
+++ Aristoruta/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'MenuP',
     'Rutas',
+    'QuienesSomos',
 ]
 
 MIDDLEWARE = [
--- Aristoruta/urls.py
+++ Aristoruta/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from django.urls import include
 from MenuP import views as menu_views
 from Rutas import views as rutas_views
+from QuienesSomos import views as quienes_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', menu_views.Menu, name='home'),
     path('Rutas/', rutas_views.Rutas, name='rutas'),
+    path('QuienesSomos/', quienes_views.Quienes, name='quienes'),
 ]
